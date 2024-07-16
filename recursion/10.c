b628c35889cdff211a93fb845c8a6d84c911b5ed
ALEN T L <alentl5000@gmail.com>
2024-07-17 00:07:04 +0530

    Learned to implement tower of hanoi using recursion

--- recursion/10.c
+++ recursion/10.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() { return 0; }
+void hanoi(int n, char source, char temp, char dest);
+
+int main() {
+  int n;
+
+  printf("Enter the number of disks: ");
+  scanf("%d", &n);
+
+  hanoi(n, 'A', 'B', 'C');
+  return 0;
+}
+
+void hanoi(int n, char source, char temp, char dest) {
+  if (n == 1) {
+    printf("Move disk %d from %c -> %c\n", n, source, dest);
+    return;
+  }
+
+  hanoi(n - 1, source, dest, temp);
+  printf("Move disk %d from %c -> %c\n", n, source, dest);
+  hanoi(n - 1, temp, source, dest);
+}
