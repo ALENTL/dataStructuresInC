dcc31a04e34b97e9ceecc861497eb6620af8cecb
ALEN T L <alentl5000@gmail.com>
2024-07-21 15:32:00 +0530

    Formatted the file

--- recursion/1.c
+++ recursion/1.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int factorial(int num) {
-    if (num <= 1) {
-        return 1;
-    }
+  if (num <= 1) {
+    return 1;
+  }
 
-    return num * factorial(num-1);
+  return num * factorial(num - 1);
 }
 
 int main() {
-    int num;
+  int num;
 
-    printf("Enter the number to check factorial: ");
-    scanf("%d", &num);
+  printf("Enter the number to check factorial: ");
+  scanf("%d", &num);
 
-    printf("The factorial of num is %d\n", factorial(num));
+  printf("The factorial of num is %d\n", factorial(num));
 
-    return 0;
+  return 0;
 }
